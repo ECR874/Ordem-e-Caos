16114a5b3e8af1bc2f476acbea42bdae4597a724
Paulo Ricardo Delai <pauloricardodelai@gmail.com>
2025-11-25 21:12:25 -0300

    Spawn Aprimorado
    Novo sistema de spawn mais interativo, mais de um tipo de inimigo pode nascer por wave, em grupos e com tempos de spawn diferentes entre eles

--- Assets/Scripts/Wave/Spawner.cs
+++ Assets/Scripts/Wave/Spawner.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
@@ -8,13 +8,14 @@ public class Spawner : MonoBehaviour
     public static event Action<int> OnWaveChanged;
 
     [SerializeField] private WaveData[] waves;
-    private int _currentWaveIndex = 0; 
+    private int _currentWaveIndex = 0;
     private int _waveCounter = 0;
     private WaveData CurrentWave => waves[_currentWaveIndex];
     
-    private float _spawnTime;
-    private float _spawnCounter;
     private int _enemiesRemoved;
+    private float _wavePause = 2f;
+    private float _waveCooldown;
+    private bool _betweenWaves = false;
     
     [SerializeField] private ObjectPooler antimatterPool;
     [SerializeField] private ObjectPooler conglomeratePool;
@@ -22,14 +23,17 @@ public class Spawner : MonoBehaviour
     [SerializeField] private ObjectPooler legionPool;
     [SerializeField] private ObjectPooler remnantPool;
     [SerializeField] private ObjectPooler voidHoundPool;
-    
+
     private Dictionary<EnemyType, ObjectPooler> _poolDictionary;
 
-    private float _wavePause = 2f;
-    private float _waveCooldown;
-    private float _extraEnemies = 0;
-    private bool _betweenWaves = false;
+    private List<EnemyType> _enemies;
+    private int _currentGroupIndex;
+    private int _index;
+    private float _spawnTimer;
+    private float _groupDelay;
+    private int _totalEnemiesInWave;
     
+
     private void Awake()
     {
         _poolDictionary = new Dictionary<EnemyType, ObjectPooler>()
@@ -48,7 +52,7 @@ public class Spawner : MonoBehaviour
         Enemy.OnEnemyReachedEnd += HandleEnemyReachedEnd;
         Enemy.OnEnemyDestroyed += HandleEnemyDestroyed;
     }
-    
+
     private void OnDisable()
     {
         Enemy.OnEnemyReachedEnd -= HandleEnemyReachedEnd;
@@ -58,8 +62,54 @@ public class Spawner : MonoBehaviour
     private void Start()
     {
         OnWaveChanged?.Invoke(_waveCounter);
+        PrepareWave();
     }
-    
+
+    private void PrepareWave()
+    {
+        _currentWaveIndex = 0;
+        _enemies = new List<EnemyType>();
+        _index = 0;
+        _enemiesRemoved = 0;
+        _totalEnemiesInWave = 0;
+
+        foreach(var group in CurrentWave.groups) 
+            foreach(var sg in group.subGroups)
+                _totalEnemiesInWave += sg.count;
+
+        PrepareGroup();
+    }
+
+    private void PrepareGroup()
+    {
+        if (_currentGroupIndex >= CurrentWave.groups.Length)
+        {
+            return;
+        }
+        
+        _enemies = new List<EnemyType>();
+        _index = 0;
+        
+        var group = CurrentWave.groups[_currentGroupIndex];
+
+        foreach(var sg in group.subGroups)
+        {
+            for(int i = 0; i < sg.count; i++)
+            {
+                _enemies.Add(sg.enemyType);
+            }
+        }
+
+        for (int i = 0; i < _enemies.Count; i++)
+        {
+            int r = UnityEngine.Random.Range(0, _enemies.Count);
+            (_enemies[i], _enemies[r]) = (_enemies[r], _enemies[i]);
+        }
+
+        _spawnTimer = 0;
+        _groupDelay = group.groupDelay;
+    }
+
     void Update()
     {
         if (_betweenWaves)
@@ -70,50 +120,63 @@ public class Spawner : MonoBehaviour
                 _currentWaveIndex = (_currentWaveIndex + 1) % waves.Length;
                 _waveCounter++;
                 OnWaveChanged?.Invoke(_waveCounter);
-                _spawnCounter = 0;
-                _enemiesRemoved = 0;
-                _spawnTime = 0;
-                _extraEnemies = 0;
+
                 _betweenWaves = false;
             }
+            return;
         }
-        else
+
+        if (_currentGroupIndex >= CurrentWave.groups.Length)
         {
-            _spawnTime -= Time.deltaTime;
-            if (_spawnTime <= 0 && _spawnCounter < CurrentWave.numberOfEnemies + _extraEnemies)
-            {
-                _spawnTime = CurrentWave.spawnInterval;
-                SpawnEnemy();
-                _spawnCounter++;
-            }
-            else if (_spawnCounter >= CurrentWave.numberOfEnemies && _enemiesRemoved >= CurrentWave.numberOfEnemies + _extraEnemies)
+            if (_enemiesRemoved >= _totalEnemiesInWave)
             {
                 _betweenWaves = true;
+                _waveCooldown = _wavePause;
             }
+
+            return;
+        }
+
+        if (_index >= _enemies.Count)
+        {
+            _groupDelay -= Time.deltaTime;
+            if (_groupDelay <= 0)
+            {
+                _currentGroupIndex++;
+                PrepareGroup();
+            }
+
+            return;
+        }
+        
+        _spawnTimer -= Time.deltaTime;
+        if (_spawnTimer <= 0)
+        {
+            SpawnEnemy(_enemies[_index]);
+            _index++;
+            _spawnTimer = CurrentWave.spawnInterval;
+        }
+    }
+    
+    private void SpawnEnemy(EnemyType enemyType)
+    {
+        if (_poolDictionary.TryGetValue(enemyType, out var pool))
+        {
+            GameObject obj = pool.GetPooledObject();
+            obj.transform.position = transform.position;
+            obj.SetActive(true);
         }
     }
 
-    private void SpawnEnemy()
+    public void SpawnInternalEnemy(EnemyType enemyType, Vector3 pos)
     {
-        if (_poolDictionary.TryGetValue(CurrentWave.enemyType, out var pool))
+        if (_poolDictionary.TryGetValue(enemyType, out var pool))
         {
-            GameObject spawnObject = pool.GetPooledObject();
-            spawnObject.transform.position = transform.position;
-            spawnObject.SetActive(true);
-        }                                                               
-    }
-    
-    public void SpawnInternalEnemy(EnemyType EnemySpawned, Vector3 position)
-    {
-        if (_poolDictionary.TryGetValue(EnemySpawned, out var pool))
-        {
-            GameObject spawnObject = pool.GetPooledObject();
-            spawnObject.transform.position = position;
-            spawnObject.SetActive(true);
-            
+            GameObject obj = pool.GetPooledObject();
+            obj.transform.position = pos;
+            obj.SetActive(true);
         }
     }
-    
 
     private void HandleEnemyReachedEnd(EnemyData data)
     {
@@ -125,4 +188,3 @@ public class Spawner : MonoBehaviour
         _enemiesRemoved++;
     }
 }
-
--- Assets/Scripts/Wave/WaveData.cs
+++ Assets/Scripts/Wave/WaveData.cs
@@ -3,8 +3,20 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WaveData", menuName = "Scriptable Objects/WaveData")]
 public class WaveData : ScriptableObject
 {
-    public EnemyType enemyType;
-    public float spawnInterval;
-    public int numberOfEnemies;
+    [System.Serializable]
+    public class SubGroup
+    {
+        public EnemyType enemyType;
+        public int count;
+    }
 
+    [System.Serializable]
+    public class Group
+    {
+        public SubGroup[] subGroups;
+        public float groupDelay;
+    }
+    
+    public Group[] groups;
+    public float spawnInterval;
 }
